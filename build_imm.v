978049364ba7553f77323322833f66bfd8e9f780
Quit123 <88991068+Quit123@users.noreply.github.com>
2024-05-14 14:08:56 +0800

    Update build_imm.v

--- build_imm.v
+++ build_imm.v
@@ -22,39 +22,22 @@
 
 module Build_imm(
 input[31:0] instruction,
-output reg[31:0] imm32
+output[31:0] imm32
     );
-reg[11:0] imm12 = 12'b0000_0000_0000;
-reg[19:0] imm20 = 20'b0000_0000_0000_0000_0000;
-always @(*) begin
-    case (instruction[6:0])
-        7'b0010011, 7'b0000011:begin//I-format
-        imm12 = instruction[31:20];
-            if(instruction[6:0] == 7'b0000011 && (instruction[14:12] == 3'b100 || instruction[14:12] == 3'b101))begin
-            imm32 = {20'b0, imm12[11:0]};
-            end else begin
-            imm32 = {{20{imm12[11]}}, imm12[11:0]};
-            end
-        end
-        7'b1100011:begin//S-format
-        imm12 = {instruction[31:25],instruction[11:7]};
-        imm32 = {{20{imm12[11]}}, imm12[11:5]};
-        end
-        7'b0100011:begin//SB-format
-        imm12 = {instruction[31], instruction[7], instruction[30:25], instruction[11:8]};
-        imm32 = {{19{imm12[11]}}, imm12[11:0], 1'b0};
-        end
-        7'b0010111:begin//U-format
-        imm20 = instruction[31:12];
-        imm32 = {imm20[19:0], 12'b0};
-        end
-        7'b1101111:begin//UJ-format
-        imm20 = {instruction[31], instruction[19:12], instruction[20], instruction[30:21]};
-        imm32 = {{11{imm20[19]}}, imm20[19:0], 1'b0};
-        end
-        default:begin
-        imm32 = 32'b0;
-        end
-    endcase
-end
+wire[11:0] imm12;
+wire[19:0] imm20;
+assign imm12 = (instruction[6:0] == 7'b0010011 || instruction[6:0] == 7'b0000011) ? instruction[31:20] ://0010011I-format,0000011,(lw)I-format
+               (instruction[6:0] == 7'b0100011) ? {instruction[31:25],instruction[11:7]} ://S-format
+               (instruction[6:0] == 7'b1100011) ? {instruction[31], instruction[7], instruction[30:25], instruction[11:8]} ://SB-format
+               12'h000;
+assign imm20 = (instruction[6:0] == 7'b0010111) ? instruction[31:12] ://U-format
+               (instruction[6:0] == 7'b1101111) ? {instruction[31], instruction[19:12], instruction[20], instruction[30:21]} ://UJ-format
+               20'h00000;
+assign imm32 = (instruction[6:0] == 7'b0000011) ? {{20{imm12[11]}}, imm12[11:0]} ://(lw)
+               (instruction[6:0] == 7'b0010011) ? {{20{imm12[11]}}, imm12[11:0]} ://(addi)
+               (instruction[6:0] == 7'b1100011) ? {{20{imm12[11]}}, imm12[11:5]} ://(beq)计算的两行之间的差值
+               (instruction[6:0] == 7'b0100011) ? {{19{imm12[11]}}, imm12[11:0], 1'b0} ://（sw）
+               (instruction[6:0] == 7'b0010111) ? {imm20[19:0], 12'b0} ://no
+               (instruction[6:0] == 7'b1101111) ? {{11{imm20[19]}}, imm20[19:0], 1'b0} ://no
+               32'h0000_0000;
 endmodule
