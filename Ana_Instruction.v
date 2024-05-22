2db2bf432d86b29b2d3308edcee8ad31979619cb
Quit123 <88991068+Quit123@users.noreply.github.com>
2024-05-22 10:41:07 +0800

    Update Ana_Instruction.v

--- Ana_Instruction.v
+++ Ana_Instruction.v
@@ -1,48 +1,28 @@
-`timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 2024/05/09 17:30:37
-// Design Name: 
-// Module Name: Ana_Instruction
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
-
 module Ana_Instruction(
 input clk,
 input reset,
 input Branch,
 input zero,
-input ALU_result,
+input[31:0] ALU_result,
 output[31:0] Instruction
     );
-    reg[31:0] PC;
+    reg [31:0] PC; // 程序计数器
+    wire [31:0] nextPC;
     wire isBranch;
+
+    // 分支条件判断
     assign isBranch = (Branch && zero) ? 1'b1 : 1'b0;
-    always@(posedge clk)begin
-        if(reset == 1'b1)begin
-             PC <= 32'h0000_0000;
+
+    // 下一个PC值的组合逻辑
+    assign nextPC = (isBranch) ? {PC[31:2] + ALU_result[29:0], PC[1:0]}: 
+                              (PC + 32'h0000_0004);
+    always@(posedge clk, negedge reset)begin
+        if(reset == 1'b0)begin
+            PC <= 32'h0000_0000;
         end
-        case(isBranch)
-            1'b1:begin
-                PC <= PC + ALU_result;
-            end
-            1'b0:begin
-                PC <= PC + 32'h0000_0004;
-            end
-        endcase
+        else begin
+            PC <= nextPC;
+        end
     end
     prgrom urom(.clka(clk), .addra(PC[15:2]), .douta(Instruction));
 endmodule
