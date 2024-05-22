148b38ff8706ec5bf12fcaaf76cd2ae6a2614e77
Quit123 <88991068+Quit123@users.noreply.github.com>
2024-05-22 10:39:16 +0800

    Update Leds.v

--- Leds.v
+++ Leds.v
@@ -27,21 +27,21 @@ module Leds (
     input			ledcs,		// 1 means the leds are selected as output (从memorio来的，由低至高位形成的LED片选信号)//LEDCtrl
     input	[1:0]	ledaddr,	// 2'b00 means updata the low 8bits of ledout, 2'b10 means updata the high 8 bits of ledout
     input	[15:0]	ledwdata,	// the data (from register/memorio)  waiting for to be writen to the leds of the board
-    output	[7:0]	ledout		// the data writen to the leds  of the board
+    output	[15:0]	ledout		// the data writen to the leds  of the board
 );
 
-    reg [7:0] ledout_design;
+    reg [15:0] ledout_design;
     assign ledout = ledout_design;
 
-    always @ (posedge led_clk or posedge ledrst) begin
-        if (ledrst)
+    always @ (posedge led_clk or negedge ledrst) begin
+        if (ledrst == 1'b0)
             ledout_design <= 24'h000000;
 		//!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-		else if (ledcs) begin
+		else if (ledcs == 1'b1) begin
 			if (ledaddr == 2'b00)
-				ledout_design[7:0] <= {ledwdata[7:0] };
+				ledout_design <= ledwdata;
 			else if (ledaddr == 2'b10 )
-				ledout_design[7:0] <= { ledwdata[15:8]};
+				ledout_design <= ledwdata;
 			else
 				ledout_design <= ledout_design;
         end else begin
