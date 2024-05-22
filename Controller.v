9ac4caeb8094012f16952b7bc8590b05254fa0ff
Quit123 <88991068+Quit123@users.noreply.github.com>
2024-05-22 10:41:39 +0800

    Update Controller.v

--- Controller.v
+++ Controller.v
@@ -37,20 +37,19 @@ module Controller(
     output IOWrite_singal // 1 indicates I/O write
     //type:判断指令结束地点
     );
-    assign IORead_singal = (opcode == 7'b000_0011 && Alu_resultHigh == 22'b1111_1111_1111_1111_1111_11) ? 1'b1 : 1'b0;
-    assign IOWrite_singal = (opcode == 7'b010_0011 && Alu_resultHigh == 22'b1111_1111_1111_1111_1111_11) ? 1'b1 : 1'b0;//sw时判断输入地址，向设备输入，显示数码管
+    assign IORead_singal = (opcode == 7'b000_0011 && Alu_resultHigh == 22'h3fffff) ? 1'b1 : 1'b0;
+    assign IOWrite_singal = (opcode == 7'b010_0011 && Alu_resultHigh == 22'h3fffff) ? 1'b1 : 1'b0;//sw时判断输入地址，向设备输入，显示数码管
     assign MemOrIOtoReg = IORead_singal || IOWrite_singal;
     
     assign Branch = (opcode == 7'b110_0011 || opcode == 7'b110_1111) ? 1'b1 : 1'b0;//最后一个jal
     assign MemWrite = (opcode == 7'b010_0011 && IOWrite_singal == 1'b0) ? 1'b1 : 1'b0;//sw时为1，可以向mem存
     assign MemRead = (opcode == 7'b000_0011 && IORead_singal == 1'b0) ? 1'b1 : 1'b0;
     assign MemtoReg = (opcode == 7'b000_0011) ? 1'b1 : 1'b0;
-    assign RegWrite = (opcode == 7'b011_0011 || opcode == 7'b000_0011 || opcode == 7'b001_0011) ? 1'b1 : 1'b0;
+    assign RegWrite = (opcode == 7'b011_0011 || opcode == 7'b000_0011 || opcode == 7'b001_0011) ? 1'b1 : 1'b0;//最后一个时I指令
     assign ALUOp = (opcode == 7'b011_0011 || opcode == 7'b001_0011) ? 2'b10 :  //add、sub、and、or
                    (opcode == 7'b000_0011 || opcode == 7'b010_0011) ? 2'b00 :  //ld、sw
                    (opcode == 7'b110_0011) ? 2'b01 : 2'b11;//11无意义  01beq
     assign ALUSrc = (opcode == 7'b000_0011 || opcode == 7'b001_0011 || opcode == 7'b010_0011 || opcode == 7'b110_1111) ? 1'b1 : 1'b0;//最后一个jal
-    
 endmodule
             /*7'b001_0111, 7'b011_0111:begin//U aupic、lui
             Branch = 1'b0;
